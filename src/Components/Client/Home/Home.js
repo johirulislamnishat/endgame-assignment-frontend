e042db580de3bf5900484d670194ed9b46453ad9
Johirul Islam Nishat <jahirulislamn@gmail.com>
2022-03-21 00:46:53 +0600

    feature added

--- src/Components/Client/Home/Home.js
+++ src/Components/Client/Home/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
+import AppointmentHome from "../Appointments/AppointmentHome";
 import FooterMain from "../Footer/FooterMain";
 import Header from "../Headers/Header/Header";
 import TopBanner from "../Headers/TopBanner/TopBanner";
+import MiddleHospitalInfo from "../HospitalInfo/MiddleHospitalInfo";
+import TopHospitalInfo from "../HospitalInfo/TopHospitalInfo";
 
 const Home = () => {
   return (
     <>
       <Header />
       <TopBanner />
+      <TopHospitalInfo />
+      <AppointmentHome />
+      <MiddleHospitalInfo />
       <FooterMain />
     </>
   );
